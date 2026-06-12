75b754aab13704efb77f7a48d2b4d8395b872661
KAYRAN_APPS <ibrahim.kayran@g5fteknoloji.com>
2026-06-12 17:20:14 +0300

    Create __init__.py

---
+++ shared/__init__.py
@@ -0,0 +1,8 @@
+"""KAYRAN ortak modülleri"""
+   from .utils import (
+       tr_now, tr_today, tr_tomorrow, tr_yesterday,
+       tr_today_iso, tr_now_str,
+       sayfa_error_handler, safe_run,
+       vade_durumu,
+       TURKIYE_TZ,
+   )
